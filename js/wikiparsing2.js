ef13d979005bc2f0fbea9bed1cc28daa98bfccd4
sidtuli <stuli@terpmail.umd.edu>
2016-06-03 00:52:23 -0400

    Doing more wikipedia parsing

---
+++ js/wikiparsing2.js
@@ -0,0 +1,19 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('DataController',['$scope','$http',function($scope,$http){
+    $scope.name = '';
+    $scope.search = function(){
+        $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles='+$scope.name+'&rvprop=content&rvsection=0&rvcontentformat=text%2Fx-wiki&callback=JSON_CALLBACK').success(function(data){
+            $scope.data = data.query.pages;
+            angular.forEach($scope.data,function(value){
+              $scope.data = value.revisions[0]['*'];  
+            });
+            $scope.deathdate = $scope.data.match($scope.deathdatereg,"g");
+            $scope.birthdate = $scope.data.match($scope.birthdatereg,"g");
+        
+        });
+        $scope.birthdatereg = new RegExp(/{([Bb]irth date(.*?))}/,"g");
+        $scope.deathdatereg = new RegExp(/({[Dd]eath date(.*?)})/,"g");
+    };
+    
+}]);
