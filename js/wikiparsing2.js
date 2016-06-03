0dce986e262b7cdf75f1a67a4c0e489b2ed9cfba
sidtuli <stuli@terpmail.umd.edu>
2016-06-03 14:34:34 -0400

    More Wikipedia Parsing
    I believe I have improved my ability to retrieve the data I want about a
    person.

--- js/wikiparsing2.js
+++ js/wikiparsing2.js
@@ -10,10 +10,15 @@ myApp.controller('DataController',['$scope','$http',function($scope,$http){
             });
             $scope.deathdate = $scope.data.match($scope.deathdatereg,"g");
             $scope.birthdate = $scope.data.match($scope.birthdatereg,"g");
+            $scope.deathplace = $scope.data.match($scope.deathplacereg,"g");
+            $scope.birthplace = $scope.data.match($scope.birthplacereg,"g");
         
         });
         $scope.birthdatereg = new RegExp(/{([Bb]irth date(.*?))}/,"g");
         $scope.deathdatereg = new RegExp(/({[Dd]eath date(.*?)})/,"g");
+        $scope.deathplacereg = new RegExp(/death_place[ ={}A-z|,\.\u00C0-\u00FF\-]*/,"g");
+        $scope.birthplacereg = new RegExp(/birth_place[ ={}A-z|,\.\u00C0-\u00FF\-]*/,"g");
+        //new RegExp(/[ ={}\[\]A-z,\.|]*( \|)/,"g");
     };
     
 }]);
