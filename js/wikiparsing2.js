dff9f0ffdaa2da9e042f6d27cb67fb6948f7d509
sidtuli <stuli@terpmail.umd.edu>
2016-06-05 12:36:10 -0400

    Small regex changes
    I'm going to try to do more stuff with searches with multiple results
    and redirects.

--- js/wikiparsing2.js
+++ js/wikiparsing2.js
@@ -4,6 +4,7 @@ myApp.controller('DataController',['$scope','$http',function($scope,$http){
     $scope.name = '';
     $scope.search = function(){
         $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles='+$scope.name+'&rvprop=content&rvsection=0&rvcontentformat=text%2Fx-wiki&callback=JSON_CALLBACK').success(function(data){
+            $scope.info = data;
             $scope.data = data.query.pages;
             angular.forEach($scope.data,function(value){
               $scope.data = value.revisions[0]['*'];  
@@ -16,8 +17,8 @@ myApp.controller('DataController',['$scope','$http',function($scope,$http){
         });
         $scope.birthdatereg = new RegExp(/{([Bb]irth date(.*?))}/,"g");
         $scope.deathdatereg = new RegExp(/({[Dd]eath date(.*?)})/,"g");
-        $scope.deathplacereg = new RegExp(/death_place[ ={}A-z|,\.\u00C0-\u00FF\-]*/,"g");
-        $scope.birthplacereg = new RegExp(/birth_place[ ={}A-z|,\.\u00C0-\u00FF\-]*/,"g");
+        $scope.deathplacereg = new RegExp(/death_place[ ={}A-z|,\.\u00C0-\u00FF\-\(\)]*/,"g");
+        $scope.birthplacereg = new RegExp(/birth_place[ ={}A-z|,\.\u00C0-\u00FF\-\(\)]*/,"g");
         //new RegExp(/[ ={}\[\]A-z,\.|]*( \|)/,"g");
     };
     
